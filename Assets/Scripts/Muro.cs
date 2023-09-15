eaf9bbb2cdc84881b219a846b7e10d3c8ead73d6
DiegoJohnsonYZ <98571062+DiegoJohnsonYZ@users.noreply.github.com>
2023-09-15 17:40:53 -0500

    square space

--- Assets/Scripts/Muro.cs
+++ Assets/Scripts/Muro.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Muro : MonoBehaviour
+public class Muros : MonoBehaviour
 {
+
+    public static Muros instance;
     
+    private void Awake()
+    {
+        instance = this;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, -0.02f, 0);
+
     }
 }
