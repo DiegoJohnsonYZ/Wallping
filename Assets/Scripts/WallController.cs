867bbbe4a1d6f3ce79d5c7a1556de01b520dcaf5
luis-gl <42353343+luis-gl@users.noreply.github.com>
2023-09-15 23:35:52 -0500

    wall masking

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    public Sprite maskSprite;
+    
+    private SpriteRenderer _wallRenderer;
+    private SpriteMask _wallMask;
+
+    private void OnValidate()
+    {
+        Setup();
+    }
+
+    public void Setup()
+    {
+        if (!_wallMask) _wallMask = GetComponentInChildren<SpriteMask>();
+        if (!_wallRenderer) _wallRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        _wallMask.sprite = maskSprite;
+        _wallRenderer.maskInteraction = maskSprite ? SpriteMaskInteraction.VisibleInsideMask : SpriteMaskInteraction.None;
+    }
+}
