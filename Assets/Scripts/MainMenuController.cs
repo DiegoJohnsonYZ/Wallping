dfd29e1dddaa88b0efbd16f3bc2f47d38e09800a
luis-gl <42353343+luis-gl@users.noreply.github.com>
2023-09-17 04:00:27 -0500

    main menu created

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private GameObject mainMenuContainer;
+    [SerializeField] private GameObject creditsContainer;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(StartGame);
+        creditsButton.onClick.AddListener(ShowCredits);
+        backButton.onClick.AddListener(ShowMainMenu);
+    }
+
+    private void StartGame()
+    {
+        StartCoroutine(LoadGameScene());
+    }
+
+    private IEnumerator LoadGameScene()
+    {
+        var asyncOperation = SceneManager.LoadSceneAsync("MainGame");
+        asyncOperation.allowSceneActivation = false;
+
+        while (asyncOperation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        asyncOperation.allowSceneActivation = true;
+    }
+
+    private void ShowCredits()
+    {
+        mainMenuContainer.SetActive(false);
+        creditsContainer.SetActive(true);
+    }
+
+    private void ShowMainMenu()
+    {
+        mainMenuContainer.SetActive(true);
+        creditsContainer.SetActive(false);
+    }
+}
